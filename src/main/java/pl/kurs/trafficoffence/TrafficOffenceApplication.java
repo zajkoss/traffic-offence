a275126abefa5e7289ac5d4023ee52f5b568e736
Lukasz <lukasz100.96@gmail.com>
2022-06-18 18:44:31 +0200

    Initial commit

---
+++ src/main/java/pl/kurs/trafficoffence/TrafficOffenceApplication.java
@@ -0,0 +1,13 @@
+package pl.kurs.trafficoffence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficOffenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficOffenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kurs/trafficoffence/TrafficOffenceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.trafficoffence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficOffenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
