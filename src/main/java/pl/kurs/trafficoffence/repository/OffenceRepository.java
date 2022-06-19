e707fe84c70dc3b7daa322e109b7fc9463716072
Lukasz <lukasz100.96@gmail.com>
2022-06-19 13:44:32 +0200

    Add query to sum points

--- src/main/java/pl/kurs/trafficoffence/repository/OffenceRepository.java
+++ src/main/java/pl/kurs/trafficoffence/repository/OffenceRepository.java
@@ -1,8 +1,14 @@
 package pl.kurs.trafficoffence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.kurs.trafficoffence.model.Offence;
+import pl.kurs.trafficoffence.model.Person;
+
+import java.time.LocalDateTime;
 
 public interface OffenceRepository extends JpaRepository<Offence, Long> {
 
+    @Query("SELECT SUM(o.points) FROM Offence o where o.person = ?1 and o.time >= ?2")
+    long sumPointsByPeselAndTime(Person person, LocalDateTime time);
 }
