ef4eb3b121c1b45ea23b12b285aa91d124c1278a
Lukasz <lukasz100.96@gmail.com>
2022-06-19 13:44:50 +0200

    Add query to check exist person by pesel

--- src/main/java/pl/kurs/trafficoffence/repository/PersonRepository.java
+++ src/main/java/pl/kurs/trafficoffence/repository/PersonRepository.java
@@ -3,6 +3,11 @@ package pl.kurs.trafficoffence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kurs.trafficoffence.model.Person;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    boolean existsByPesel(String peselNumber);
+    Optional<Person> findByPesel(String peselNumber);
 
 }
