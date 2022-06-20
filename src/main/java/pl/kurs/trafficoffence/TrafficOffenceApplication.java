963c9ec1ae1fce6b841bb5ed7667ba14c98a1502
Lukasz <lukasz100.96@gmail.com>
2022-06-20 07:58:44 +0200

    Fix async. annotation

--- src/main/java/pl/kurs/trafficoffence/TrafficOffenceApplication.java
+++ src/main/java/pl/kurs/trafficoffence/TrafficOffenceApplication.java
@@ -2,10 +2,10 @@ package pl.kurs.trafficoffence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@Async
+@EnableAsync
 public class TrafficOffenceApplication {
 
     public static void main(String[] args) {
