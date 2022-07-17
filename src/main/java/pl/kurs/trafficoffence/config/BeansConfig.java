ae42ab407fd0a4edcd5f077dc8e617eaeebc74bd
Lukasz <lukasz100.96@gmail.com>
2022-07-17 17:16:08 +0200

    Delete unless data init

--- src/main/java/pl/kurs/trafficoffence/config/BeansConfig.java
+++ src/main/java/pl/kurs/trafficoffence/config/BeansConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,19 +12,13 @@ import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import pl.kurs.trafficoffence.model.Offence;
-import pl.kurs.trafficoffence.model.Person;
 import pl.kurs.trafficoffence.repository.OffenceRepository;
 import pl.kurs.trafficoffence.repository.PersonRepository;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Properties;
 
 @Configuration
@@ -92,23 +85,5 @@ public class BeansConfig {
                 .build();
     }
 
-
-
-    @Bean
-    public ApplicationRunner dataInitializer(OffenceRepository offenceRepository, PersonRepository personRepository) {
-        return args -> {
-            Person person1 = new Person("Jan","Kowalski","lukz1184@gmail.com","17252379565",new HashSet<>(),null);
-            Person person2 = new Person("Anna","Kowalska","lukz1184@gmail.com","93102298064",new HashSet<>(),null);
-            personRepository.save(person1);
-            personRepository.save(person2);
-            Offence offence1 = new Offence(LocalDateTime.of(2022, 6, 20, 10, 0),5, new BigDecimal("100.00"),new HashSet<>(),person1);
-            Offence offence2 = new Offence(LocalDateTime.of(2022, 6, 20, 10, 0),5, new BigDecimal("200.00"),new HashSet<>(),person1);
-            Offence offence3 = new Offence(LocalDateTime.of(2022, 6, 20, 15, 0),5, new BigDecimal("300.00"),new HashSet<>(),person1);
-            offenceRepository.saveAll(Arrays.asList(offence1,offence2,offence3));
-
-
-
-        };
-    }
 
 }
